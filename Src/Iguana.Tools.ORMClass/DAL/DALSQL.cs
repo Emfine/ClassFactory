be0b36d57b2d2b22e0a56d33f3459c46d8d82f70
Emfine <601563839@qq.com>
2020-02-21 12:50:29 +0800

    ntext映射

--- Src/Iguana.Tools.ORMClass/DAL/DALSQL.cs
+++ Src/Iguana.Tools.ORMClass/DAL/DALSQL.cs
@@ -11,21 +11,6 @@ namespace Iguana.Tools.ORMClass.DAL
 {
     internal class DALSQL : IDAL
     {
-        public List<TableFieldDTO> GetTableFieldList(TableFieldCDT condition)
-        {
-            string sql = @"SELECT a.name AS Name,
-                                               TYPE_NAME(a.user_type_id) AS Type,
-	                                           b.value AS Note,
-                                               a.max_length AS Length,
-                                               a.is_nullable AS IsNullable,
-                                               a.is_identity AS IsIdentity
-                                        FROM sys.all_columns AS a
-                                        LEFT JOIN sys.extended_properties AS b ON b.major_id = a.object_id AND b.minor_id = a.column_id
-                                        WHERE a.object_id = OBJECT_ID(@Table)
-                                        ORDER BY a.column_id;";
-            var list = DataHelper.ConnectionFactory(condition.ConnectionString).Query<TableFieldDTO>(sql, condition).ToList();
-            return list;
-        }
         public Dictionary<string, string> GetDBTypeCSharpDic()
         {
             var dic = new Dictionary<string, string>
@@ -45,10 +30,27 @@ namespace Iguana.Tools.ORMClass.DAL
                 ["datetime"] = "DateTime",
                 ["uniqueidentifier"] = "Guid",
                 ["text"] = "string",
+                ["ntext"] = "string",
                 ["image"] = "byte[]",
                 ["binary"] = "byte[]"
             };
             return dic;
+        }
+
+        public List<TableFieldDTO> GetTableFieldList(TableFieldCDT condition)
+        {
+            string sql = @"SELECT a.name AS Name,
+                                               TYPE_NAME(a.user_type_id) AS Type,
+	                                           b.value AS Note,
+                                               a.max_length AS Length,
+                                               a.is_nullable AS IsNullable,
+                                               a.is_identity AS IsIdentity
+                                        FROM sys.all_columns AS a
+                                        LEFT JOIN sys.extended_properties AS b ON b.major_id = a.object_id AND b.minor_id = a.column_id
+                                        WHERE a.object_id = OBJECT_ID(@Table)
+                                        ORDER BY a.column_id;";
+            var list = DataHelper.ConnectionFactory(condition.ConnectionString).Query<TableFieldDTO>(sql, condition).ToList();
+            return list;
         }
     }
 }
